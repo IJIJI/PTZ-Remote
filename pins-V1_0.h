01f98aa291947c37222fc685d27c221b4e06724f
IJIJI <ijsslkstr@gmail.com>
2020-09-09 14:45:35 +0000

    added semi pins.h

---
+++ pins-V1_0.h
@@ -0,0 +1,18 @@
+#define joyXPin 
+#define joyYPin
+#define joyZPin
+
+
+#define radioCE
+#define radioCSN
+
+#define row1 
+#define row2
+#define row3
+#define row4
+
+#define col1
+#define col2
+#define col3
+
+#define deadZone 150
